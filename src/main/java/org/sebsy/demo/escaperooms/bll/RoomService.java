3d5d2d2d4ddc71c87ba61fe482f666fe2026313b
Aesk0-git <nl.crssn@gmail.com>
2026-04-14 16:40:01 +0200

    TP02-Advanced

--- src/main/java/org/sebsy/demo/escaperooms/bll/RoomService.java
+++ src/main/java/org/sebsy/demo/escaperooms/bll/RoomService.java
@@ -1,5 +1,10 @@
 package org.sebsy.demo.escaperooms.bll;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface RoomService {
 
     String entreeSalle();
